7edce7f03bd50b21053c8612bf825dc9a63bda7b
Rohan Kokane <rohankokane19@gmail.com>
2022-09-03 12:48:43 +0530

    slide animations

--- src/components/stepperForm/StepperForm.jsx
+++ src/components/stepperForm/StepperForm.jsx
@@ -1,15 +1,24 @@
-import { NameForm } from 'components/form/NameForm'
-import { Step, Stepper } from 'components/stepper'
 import { useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import 'components/form/form.css'
+import { Step, Stepper } from 'components/stepper'
+import { NameForm } from 'components/form/NameForm'
 
 export const StepperForm = () => {
 	const [step, setStep] = useState(0)
+	const [prevStep, setPrevStep] = useState(0)
 	const handleStepClick = (n) => {
+		setPrevStep(step)
 		setStep(n)
 	}
+	const direction = prevStep < step ? 'forward' : 'backward'
+	const slide = {
+		'--slide-in': `${direction === `forward` ? 'translateX(100%)' : 'translateX(-100%)'}`,
+		'--slide-out': `${direction === `forward` ? 'translateX(-100%)' : 'translateX(100%)'}`,
+	}
+
 	return (
-		<div>
+		<div className='stepper-form-container' style={slide}>
 			<Stepper currentStep={step}>
 				{[...new Array(4)].map((_, i) => (
 					<Step
@@ -20,7 +29,26 @@ export const StepperForm = () => {
 					></Step>
 				))}
 			</Stepper>
-			<NameForm fullName={'steve'} displayName='ste' />
+			<FormWrapper active={step === 0}>
+				<NameForm fullName={'steve'} displayName='ste' />
+			</FormWrapper>
+			<FormWrapper active={step === 1}>
+				<NameForm fullName={'stark'} displayName='tony' />
+			</FormWrapper>
+			<FormWrapper active={step === 2}>
+				<NameForm fullName={'stark'} displayName='tony' />
+			</FormWrapper>
+			<FormWrapper active={step === 3}>
+				<NameForm fullName={'stark'} displayName='tony' />
+			</FormWrapper>
 		</div>
+	)
+}
+
+const FormWrapper = ({ active, children }) => {
+	return (
+		<CSSTransition in={active} timeout={500} classNames='form__container' unmountOnExit>
+			{children}
+		</CSSTransition>
 	)
 }
